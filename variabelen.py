56288aa741fbd8b38fa28bba4eb4d61385325e64
Kipmevrouw <26923@ma-web.nl>
2020-09-16 14:31:55 +0200

    Play with python

---
+++ variabelen.py
@@ -0,0 +1,16 @@
+# Handig, dat commentaar
+naam = "Becky"		#Dit is een string
+aanUit = True		#Dit is een boolean
+puntGetal = 5.5		#Dit is een float en double
+karakter = '#'		#Dit is een character
+heelGetal = 123		#Dit is een integer
+
+# puntGetal = puntGetal + 2
+puntGetal = 2
+
+restWaarde = puntGetal % 3
+
+print(naam)
+print(aanUit)
+print(puntGetal)
+print(restWaarde)
