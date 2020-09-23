cd2ea59bbcf338b8ad9d38893bcc32b2fc7134f1
Kipmevrouw <26923@ma-web.nl>
2020-09-23 14:43:15 +0200

    Python versie

---
+++ PYTB1L3DailyChoices.py
@@ -0,0 +1,67 @@
+print("1. Hoe oud ben ik?")
+print("A. 50")
+print("B. 36")
+print("C. 16")
+print("D. 55")
+print("E. 15")
+antwoord = input("Kies A, B, C, D of E >>>>")
+if(antwoord == "A"):
+	print("fout")
+elif(antwoord == "B"):
+	print("Fout!")
+elif(antwoord == "C"):
+	print("Correct")
+elif(antwoord == "D"):
+	print("Fout!")
+elif(antwoord == "E"):
+	print("Fout!")
+
+print("2. Wat is mijn lieverling kleur.")
+print("A. Paars")
+print("B. Geel")
+print("C. Zwart")
+antwoord = input("Kies A, B, of C >>>>")
+if(antwoord == "A"):
+	print("Correct")
+elif(antwoord == "B"):
+	print("Fout!")
+elif(antwoord == "C"):
+	print("Fout!")
+
+
+print("3. Hoeveel vingers steek ik op?")
+print("A. 1")
+print("B. 5")
+print("C. 3")
+antwoord = input("Kies A, B, of C >>>>")
+if(antwoord == "A"):
+	print("Fout!")
+elif(antwoord == "B"):
+	print("Correct!")
+elif(antwoord == "C"):
+	print("Fout!")
+
+print("4. Welk merk is nou echt tof")
+print("A. Primark")
+print("B. Pindakaas calve")
+print("C. Samsung")
+antwoord = input("Kies A, B, of C >>>>")
+if(antwoord == "A"):
+	print("Fout!")
+elif(antwoord == "B"):
+	print("Correct!")
+elif(antwoord == "C"):
+	print("Fout!")
+
+print("5. Wat is het weer vandaag?")
+print("A. Zonnig")
+print("B. Sneeuw")
+print("C. Regenachtige dag")
+antwoord = input("Kies A, B, of C >>>>")
+if(antwoord == "A"):
+	print("Fout!")
+elif(antwoord == "B"):
+	print("Fout!")
+elif(antwoord == "C"):
+	print("Correct!")
+
