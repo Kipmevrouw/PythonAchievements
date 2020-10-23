dc9fa9aee6fea0675a30b2fe9f3bec54e8dc9ddd
Kipmevrouw <26923@ma-web.nl>
2020-10-23 11:45:08 +0200

    Update

---
+++ PYTB1L6CanYouHandleTheList.py
@@ -0,0 +1,10 @@
+lijstje1 = [1, True, "hoi", 3966, "viertig"]
+
+print(lijstje1)
+
+lijstje1.append("hans")
+print(lijstje1)
+lijstje1.remove("hoi")
+print(lijstje1)
+lijstje1.insert(6,6)
+print(lijstje1)
